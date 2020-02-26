7f4c513369eed6887d5a4f2a7432d694587b6dfc
Liu Dairui <rxhjxja@icloud.com>
2020-02-26 18:36:38 +0800

    update

--- dataset/single_character/make_directory.py
+++ dataset/single_character/make_directory.py
@@ -24,7 +24,7 @@ def main():
     type_dirs = [oracle_dir, bronze_dir, liu_dir, shuo_dir]
     mk_dirs(format_dirs)
     for format_dir in format_dirs:
-        mk_dirs([format_dir+type_dir for type_dir in type_dirs])
+        mk_dirs([format_dir + type_dir for type_dir in type_dirs])
     characters_ls = os.listdir(raw_data_dir)
     characters_dic = {}
     index = 0
@@ -39,17 +39,21 @@ def main():
     for chara_dir in os.scandir(raw_data_dir):
         # find the index of this character in dictionary
         file_no = characters_dic[chara_dir.name]
-        if not chara_dir.is_dir(): continue
+        if not chara_dir.is_dir():
+            continue
         for type_dir in os.scandir(chara_dir.path):
-            if type_dir.name not in path_mapping: continue
+            if type_dir.name not in path_mapping:
+                continue
             sub_dir = path_mapping[type_dir.name]
             for format_dir in os.scandir(type_dir.path):
-                if not format_dir.is_dir(): continue
+                if not format_dir.is_dir():
+                    continue
                 file_index = 1
                 for old_file in os.scandir(format_dir.path):
                     old_file_name = old_file.name
                     # new file path with format: typePath/J(B/L/S)1_1.png
-                    new_file_path = "%s/%s%s%s_%s%s" % (format_dir.name, sub_dir, old_file_name[0], file_no, file_index, old_file_name[-4:])
+                    new_file_path = "%s/%s%s%s_%s%s" % (format_dir.name, sub_dir, old_file_name[0], file_no, file_index,
+                                                        old_file_name[-4:])
                     print("%s =>> %s" % (old_file.path, new_file_path))
                     shutil.copyfile(old_file.path, new_file_path)
                     file_index += 1
