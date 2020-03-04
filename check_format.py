551b6ae2a628692075170af607747d4758e7bc10
Liu Dairui <rxhjxja@icloud.com>
2020-03-04 19:15:43 +0800

    fix bug

--- check_format.py
+++ check_format.py
@@ -2,18 +2,13 @@ import os
 
 
 def main():
-    # check_directories = ["base", "data_loader", "logger", "model", "trainer", "dataset"]
     # for directory in check_directories:
     for root, dirs, files in os.walk(".", topdown=False):
         for name in files:
-            python_file = os.path.join(root, name)
-            if python_file.endswith(".py"):
-                os.system("flake8 " + python_file)
-        for name in dirs:
-            if name == "dataset": continue
-            python_file = os.path.join(root, name)
-            if python_file.endswith(".py"):
-                os.system("flake8 " + python_file)
+            file = os.path.join(root, name)
+            if file.endswith(".py"):
+                print("check", file)
+                os.system("flake8 " + file)
 
 
 if __name__ == "__main__":
